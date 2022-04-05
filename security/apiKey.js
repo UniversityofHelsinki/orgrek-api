3bc210e77e60e572dc86959a4928720ba8dca9c1
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-04-05 15:47:52 +0300

    changed x-api-key to api-key

--- security/apiKey.js
+++ security/apiKey.js
@@ -1,6 +1,6 @@
 export const checkApiKey = (req, res, next) => {
     const apiKey = req.get('api-key');
-    if (!apiKey || apiKey !== process.env.HY_APPLICATION_API_KEY) {
+    if (!apiKey || apiKey !== process.env.HY_ORGANISATION_APPLICATION_API_KEY) {
         res.status(401).json({error: 'unauthorised'})
     } else {
         next();
