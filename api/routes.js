bcdf394db924c255f93abe002f3e8118627c8ebf
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-04-06 12:52:41 +0300

    add versioning to constants.js

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,8 @@
 import { texts } from '../api/api-db.js';
+import { API_VERSION_1 } from '../utils/constants.js';
 
 const router = (router) => {
-    router.get('/v1/texts', texts);
+    router.get(`/${API_VERSION_1}/texts`, texts);
 };
 
 export default router;
---
+++ utils/constants.js
@@ -0,0 +1,2 @@
+export const API_VERSION_1 = 'v1';
+export const API_VERSION_2 = 'v2';
