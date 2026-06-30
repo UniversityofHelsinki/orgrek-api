6cfd1511ef6bb73cc264f2afde93b141750b8efa
Riku Hänninen <riku.hanninen@helsinki.fi>
2026-06-30 07:43:32 +0300

    OR-1324 -- tutkimusryhmat

--- api/api-db.js
+++ api/api-db.js
@@ -86,6 +86,18 @@ export const researchResources = async (req, res) => {
     }
 };
 
+export const researchGroups = async (req, res) => {
+    try {
+      const response = await fetch(`${apiDbHost}/api/public/researchGroups`, {
+        method: 'GET',
+      });
+      const data = await response.json();
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+    }
+};
+
 export const financeUnits = async (req, res) => {
     try {
       const response = await fetch(`${apiDbHost}/api/public/financeUnits`, {
--- api/routes.js
+++ api/routes.js
@@ -1,4 +1,4 @@
-import { steeringGroups, degreeProgrammes, humanResources, humanResourcesIamGroupPrefix, texts, researchResources, financeUnits, financeUnitsPublic, educationUnits, educationUnitsV2, officialUnits, officialUnitsV2, concernGroups, financeUnitsV2, financeUnitsWithUniqueCodeExclusive, NodesInMultipleHierarchies, FinanceAndOldResearch, personnelUnitMap, konserniUnitMap, allNodeAttributes, allSuccessorsAndPredecessors, humanResourcesWithLevel } from "./api-db.js";
+import { steeringGroups, degreeProgrammes, humanResources, humanResourcesIamGroupPrefix, texts, researchResources, researchGroups, financeUnits, financeUnitsPublic, educationUnits, educationUnitsV2, officialUnits, officialUnitsV2, concernGroups, financeUnitsV2, financeUnitsWithUniqueCodeExclusive, NodesInMultipleHierarchies, FinanceAndOldResearch, personnelUnitMap, konserniUnitMap, allNodeAttributes, allSuccessorsAndPredecessors, humanResourcesWithLevel } from "./api-db.js";
 import { API_VERSION_1, API_VERSION_2 } from '../utils/constants.js';
 import { readFileSync } from "fs";
 
@@ -23,6 +23,7 @@ const router = (router) => {
     router.get(`/${API_VERSION_1}/humanResourcesIamGroupPrefix`, humanResourcesIamGroupPrefix);
     router.get(`/${API_VERSION_1}/humanResourcesWithLevel`, humanResourcesWithLevel);
     router.get(`/${API_VERSION_1}/researchResources`, researchResources);
+    router.get(`/${API_VERSION_1}/researchGroups`, researchGroups);
     router.get(`/${API_VERSION_1}/financeUnits`, financeUnits);
     router.get(`/${API_VERSION_2}/financeUnits`, financeUnitsV2);
     router.get(`/${API_VERSION_1}/financeUnitsPublic`, financeUnitsPublic);
